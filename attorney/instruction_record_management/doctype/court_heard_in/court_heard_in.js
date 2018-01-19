ce27f17d2678fda4969b4b9fb210d717933c55db
Unknown <kruger.engelbrecht@gmail.com>
2018-01-19 19:53:55 +0200

    Delete modules

--- attorney/instruction_record_management/doctype/court_heard_in/court_heard_in.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, Kruger Engelbrecht and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Court Heard In', {
-	refresh: function(frm) {
-
-	}
-});
--- attorney/instruction_record_management/doctype/file_type/file_type.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, Kruger Engelbrecht and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('File Type', {
-	refresh: function(frm) {
-
-	}
-});
