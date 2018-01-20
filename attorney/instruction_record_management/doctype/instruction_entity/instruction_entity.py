f486b5fa9776a49d3e9852f8023079792623ec7c
Unknown <kruger.engelbrecht@gmail.com>
2018-01-20 09:57:57 +0200

    Autoname amendmend

--- attorney/instruction_record_management/doctype/instruction_entity/instruction_entity.py
+++ attorney/instruction_record_management/doctype/instruction_entity/instruction_entity.py
@@ -6,5 +6,22 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+# class InstructionEntity(Document):
+# 	pass
+
+
 class InstructionEntity(Document):
 	pass
+
+
+
+	def autoname(self):
+		"""
+		Add autoname field for Instruction Entity
+
+		it used to be : field:entity_name
+		:return:
+		naming_series
+		entity_name
+		"""
+		self.name = self.naming_series + '-' + self.entity_name
