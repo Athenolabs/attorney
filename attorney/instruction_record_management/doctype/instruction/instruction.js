9a6ebffbe8c2f59c8588e5560633f3d780a8408f
Unknown <kruger.engelbrecht@gmail.com>
2018-02-17 22:17:28 +0200

    Add custom button from Instruction to Project

--- attorney/instruction_record_management/doctype/instruction/instruction.js
+++ attorney/instruction_record_management/doctype/instruction/instruction.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Instruction', {
-	refresh: function(frm) {
+    refresh: function (frm) {
+        if (frm.doc.project) {
+            // cur_frm.add_custom_button(_('button1'), function()
+            frm.add_custom_button(__(frm.doc.project), function () {
+                frappe.set_route("Form", "Project", frm.doc.project);
 
-	}
+            });
+        }
+
+    }
 });
---
+++ attorney/instruction_record_management/doctype/type_of_instruction/type_of_instruction.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, KEA and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Type of Instruction', {
+	refresh: function(frm) {
+
+	}
+});
