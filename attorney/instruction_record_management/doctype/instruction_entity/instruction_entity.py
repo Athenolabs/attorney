e8f2f966b0927419794f0c0962e850dcb52e9c59
Unknown <kruger.engelbrecht@gmail.com>
2018-02-17 09:15:03 +0200

    Amend instruction

--- attorney/instruction_record_management/doctype/instruction_entity/instruction_entity.py
+++ attorney/instruction_record_management/doctype/instruction_entity/instruction_entity.py
@@ -21,39 +21,40 @@ from frappe.model.naming import make_autoname
 class InstructionEntity(Document):
     pass
 
-    def autoname(self):
-        """
-        Add autoname field for Instruction Entity
-
-        it used to be : field:entity_name
-        :return:
-        naming_series
-        entity_name
-        """
-        # self.name = self.naming_series + '-' + self.entity_name
-        # self.name = make_autoname(self.entity_name + ' (ENT' + '/.#####')
-
-        a_now = utils.nowdate()
-        datee = datetime.datetime.strptime(a_now, "%Y-%m-%d")
-        a_month = datee.month
-        a_day = datee.day
-        a_year = datee.year
-
-        a = re.sub(r'\s+', '', self.entity_name.__str__().upper())
-        a = re.sub(r'\s+', '', a)
-        b = a[0:3]
-
-        c = '%s - %s/%s%s' % (self.entity_name, b, a_month, a_year)
-        # self.name = make_autoname(c + '/.#####')
-        # self.name = make_autoname('.#####')
-        d = make_autoname('.#####')
-
-        self.internal_reference = '%s/%s' % (c, d)
-        self.name = self.internal_reference
-
-
-
-    def validate(self):
-        if not self.internal_reference:
-            self.internal_reference = self.name
-
+    # def autoname(self):
+    #     """
+    #     Add autoname field for Instruction Entity
+    #
+    #     it used to be : field:entity_name
+    #     :return:
+    #     naming_series
+    #     entity_name
+    #     """
+    #     # self.name = self.naming_series + '-' + self.entity_name
+    #     # self.name = make_autoname(self.entity_name + ' (ENT' + '/.#####')
+    #
+    #     a_now = utils.nowdate()
+    #     datee = datetime.datetime.strptime(a_now, "%Y-%m-%d")
+    #     a_month = datee.month
+    #     a_day = datee.day
+    #     a_year = datee.year
+    #
+    #     a = re.sub(r'\s+', '', self.entity_name.__str__().upper())
+    #     a = re.sub(r'\s+', '', a)
+    #     b = a[0:3]
+    #
+    #     c = '%s - %s/%s%s' % (self.entity_name, b, a_month, a_year)
+    #     # self.name = make_autoname(c + '/.#####')
+    #     # self.name = make_autoname('.#####')
+    #     d = make_autoname('.#####')
+    #
+    #     self.internal_reference = '%s/%s' % (c, d)
+    #     # self.name = self.internal_reference
+    #     self.name = self.internal_reference
+    #
+    #
+    #
+    # def validate(self):
+    #     if not self.internal_reference:
+    #         self.internal_reference = self.name
+#
