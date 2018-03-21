064b1095932b353e042300a5e6cd17fa027eced8
Unknown <kruger.engelbrecht@gmail.com>
2018-03-21 11:58:00 +0200

    Amend instruction list.js

---
+++ attorney/instruction_record_management/doctype/instruction/instruction_list.js
@@ -0,0 +1,16 @@
+
+
+frappe.listview_settings['Instruction'] = {
+	add_fields: ["client", "file_description", "our_reference"],
+	filters:[["instruction_status","=", "Open"]],
+
+    // onload: function(me) {
+    //     frappe.route_options = {
+    //         "instruction_status": "Open"
+    //     };
+    // }
+};
+
+
+
+
