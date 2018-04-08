bdf65c554cc5cd82da44c3fbaffc8fea43dbe525
Unknown <kruger.engelbrecht@gmail.com>
2018-04-08 09:23:47 +0200

    Work on Project / Instruction integration

--- attorney/instruction_record_management/doctype/instruction/instruction.js
+++ attorney/instruction_record_management/doctype/instruction/instruction.js
@@ -7,9 +7,37 @@ frappe.ui.form.on('Instruction', {
             // cur_frm.add_custom_button(_('button1'), function()
             frm.add_custom_button(__(frm.doc.project), function () {
                 frappe.set_route("Form", "Project", frm.doc.project);
-
             });
         }
 
     }
+
+
 });
+
+// // This works but not for "reference_name"
+// frappe.ui.form.on("ToDo", "description", function (frm, cdt, cdn) {
+//     d = locals[cdt][cdn];
+//     d.description = 'Hallo World';
+//     d.reference_type = "Instruction";
+//     // LINKFIELD --
+//     // CORRESPONDING SOURCE FIELD --
+//     // TARGET FIELD --
+//     // frm.add_fetch("", "", "")
+//     d.reference_name = frm.add_fetch("parent", "name", "reference_name")
+// });
+
+
+
+
+
+
+
+
+    // LINKFIELD --
+    // CORRESPONDING SOURCE FIELD --
+    // TARGET FIELD --
+    // frm.add_fetch("", "", "")
+    // d.reference_name = .name
+
+
--- attorney/instruction_record_management/doctype/instruction/instruction_list.js
+++ attorney/instruction_record_management/doctype/instruction/instruction_list.js
@@ -1,16 +1,9 @@
 
-
 frappe.listview_settings['Instruction'] = {
-	add_fields: ["client", "file_description", "our_reference"],
-	filters:[["instruction_status","=", "Open"]],
+    add_fields: ["client", "file_description", "our_reference", "instruction_status", "date_instruction_accepted"],
+    filters: [["instruction_status", "=", "Open"]],
 
-    onload: function(me) {
-        frappe.route_options = {
-            "instruction_status": "Open"
-        };
+    onload: function (me) {
+        frappe.route_options = {"instruction_status": "Open"}
     }
 };
-
-
-
-
