ecbbdbe6b4c2212c904d90168981748a604d6abe
Unknown <kruger.engelbrecht@gmail.com>
2018-04-21 20:34:37 +0200

    Remove todo

--- attorney/instruction_record_management/doctype/instruction/instruction.js
+++ attorney/instruction_record_management/doctype/instruction/instruction.js
@@ -8,6 +8,7 @@ frappe.ui.form.on('Instruction', {
             frm.add_custom_button(__(frm.doc.project), function () {
                 frappe.set_route("Form", "Project", frm.doc.project);
             });
+
         }
 
     }
@@ -15,17 +16,18 @@ frappe.ui.form.on('Instruction', {
 
 });
 
-// // This works but not for "reference_name"
-// frappe.ui.form.on("ToDo", "description", function (frm, cdt, cdn) {
-//     d = locals[cdt][cdn];
-//     d.description = 'Hallo World';
-//     d.reference_type = "Instruction";
-//     // LINKFIELD --
-//     // CORRESPONDING SOURCE FIELD --
-//     // TARGET FIELD --
-//     // frm.add_fetch("", "", "")
-//     d.reference_name = frm.add_fetch("parent", "name", "reference_name")
-// });
+// This works but not for "reference_name"
+frappe.ui.form.on("ToDo", "description", function (frm, cdt, cdn) {
+    d = locals[cdt][cdn];
+    d.description = 'Hallo World';
+    d.reference_type = "Instruction";
+    
+    // LINKFIELD --
+    // CORRESPONDING SOURCE FIELD --
+    // TARGET FIELD --
+    // frm.add_fetch("", "", "")
+    d.reference_name = frm.add_fetch("parent", "name", "reference_name")
+});
 
 
 
